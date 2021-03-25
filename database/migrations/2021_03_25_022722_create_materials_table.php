1338270da3d135a7867de329ce332a94f6141130
Yuma Ishii <yuuma1403@gmail.com>
2021-03-25 13:26:36 +0900

    Add materials table

---
+++ database/migrations/2021_03_25_022722_create_materials_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMaterialsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('materials', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name', 50);
+            $table->text('content');
+            $table->foreignId('category_id');
+            $table->foreignId('created_by');
+            $table->foreignId('updated_by');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('materials');
+    }
+}
