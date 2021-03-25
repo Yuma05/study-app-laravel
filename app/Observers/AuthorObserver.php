d7c44fdc66a64792e4bd40d4f2ee7f45e329e294
Yuma Ishii <yuuma1403@gmail.com>
2021-03-25 16:59:27 +0900

    Add author observer

---
+++ app/Observers/AuthorObserver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Observers;
+
+use Illuminate\Database\Eloquent\Model;
+
+class AuthorObserver
+{
+    public function creating(Model $model){
+        $model->created_by = \Auth::user()->id;
+    }
+    public function updating(Model $model){
+        $model->updated_by = \Auth::user()->id;
+    }
+    public function saving(Model $model){
+        $model->updated_by = \Auth::user()->id;
+    }
+}
