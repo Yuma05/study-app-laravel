e33314322b543b3d5862155b6162d1231bb52993
Yuma Ishii <yuuma1403@gmail.com>
2021-04-12 16:05:49 +0900

    Add store request

---
+++ app/Http/Requests/Material/StoreRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Material;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\Material;
+
+class StoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|max:50',
+            'content' => 'required|String',
+            'category_id' => 'required|integer',
+        ];
+    }
+
+    public function makeMaterial(): Material
+    {
+        $material = new Material;
+        $material->fill($this->all());
+        return $material;
+    }
+}
