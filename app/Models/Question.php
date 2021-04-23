360f5b482031c5f48a1ffeca45721cfcf1d5635c
Yuma Ishii <yuuma1403@gmail.com>
2021-04-23 15:31:25 +0900

    Add fill able property and relation

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -9,4 +9,20 @@ use App\Traits\AuthorObservable;
 class Question extends Model
 {
     use HasFactory, AuthorObservable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'content',
+        'explanation',
+        'material_id',
+    ];
+
+    public function choices()
+    {
+        return $this->hasMany(Choice::class);
+    }
 }
